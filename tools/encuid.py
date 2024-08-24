fb2ae2cc163e81cf53aba974ca5ab7ec86cec9f6
up-n-atom <adam.jaremko@gmail.com>
2024-08-23 22:50:57 -0400

    Add hex output to encuid.py

--- tools/encuid.py
+++ tools/encuid.py
@@ -7,8 +7,8 @@ try:
 except ImportError:
     from Cryptodome.Cipher import AES
 
-
-LO_KEY = b'59494F4754fff00\0'
+LO_KEY = b'59494F4754fff00\0' # lower case
+UP_KEY = b'59494F4754fff01\0' # upper case
 
 if __name__ == "__main__":
     uid = b'10093f30'
@@ -17,4 +17,5 @@ if __name__ == "__main__":
     print('unique id:', uid.decode('ascii'))
     aes = AES.new(LO_KEY, AES.MODE_ECB)
     dat = aes.encrypt(uid + uid)
-    print('encrypted uid:', dat[:8].hex())
+    print('encrypted uid str:', dat[:8].hex())
+    print('encrypted uid hex:', dat[:8].hex().encode('ascii').hex())
